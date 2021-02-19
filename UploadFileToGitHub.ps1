de8aa112ffb6eb138b79ea16558462f984ced75c
yiweishih <yiwei.shih@gmail.com>
2021-02-19 15:46:10 -0800

    Finial fixed the bug at 15:46 on 2/19/2021

--- UploadFileToGitHub.ps1
+++ UploadFileToGitHub.ps1
@@ -1,43 +1,80 @@
-﻿Set-Location D:\github\WindowsTest
+﻿if (Test-Path D:\github\WindowsTest) {
+  Write-Verbose -Message "Deleting the old git repo" -Verbose
+  Set-Location D:\github\WindowsTest
+  git remote rm origin
+  Set-Location D:\github
+  Remove-Item D:\github\WindowsTest -Recurse -Force
+}
+
+if (Test-Path D:\github\PRDCA) {
+  Write-Verbose -Message "Deleting the old git repo" -Verbose
+  Set-Location D:\github\PRDCA
+  git remote rm origin
+  Set-Location D:\github
+  Remove-Item D:\github\PRDCA -Recurse -Force
+}
+
+Set-Location D:\github
+
+git clone https://github.com/yiweishih/PRDCA.git
+git clone https://github.com/yiweishih/WindowsTest.git
+
+Set-Location D:\github\WindowsTest
 
 $hostname = hostname
 
-$date = (Get-Date).AddDays(-1).ToString('MM-dd-yyyy')
+$date = get-date -Format "MM-dd-yyyy"
 
-$filename_AppList = $hostname+"_"+$date+"_AppList"
-$filename_UpdateList = $hostname+"_"+$date+"_UpdateList"
+$filename_AppList = $hostname+"_"+$date+"_AppList"+".csv"
+$filename_UpdateList = $hostname+"_"+$date+"_UpdateList"+".csv"
 
 Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Export-Csv -Path D:\github\WindowsTest\AppList.csv -NoTypeInformation
 
-if (Test-Path D:\github\WindowsTest\$filename_AppList.csv) {
-  Write-Verbose -Message "Deleting the duplicate file ($filename_AppList.csv)" -Verbose
-  git rm D:\github\WindowsTest\$filename_AppList.csv
-  git commit -m "Remove the duplicate file ($filename_AppList.csv)"
+
+if (Test-Path D:\github\WindowsTest\$filename_AppList) {
+  Write-Verbose -Message "Deleting the duplicate file ($filename_AppList)" -Verbose
+  Set-Location D:\github\WindowsTest
+  git rm "$filename_AppList"
+  git commit -m "Remove the duplicate file ($filename_AppList)"
   git push
-  #Remove-Item D:\github\WindowsTest\$filename_AppList.csv
 }
 
-Import-Csv -Path "D:\github\WindowsTest\AppList.csv" | Where-Object { $_.PSObject.Properties.Value -ne '' } | Export-Csv -Path D:\github\WindowsTest\$filename_AppList.csv -NoTypeInformation
+
+Import-Csv -Path "D:\github\WindowsTest\AppList.csv" | Where-Object { $_.PSObject.Properties.Value -ne '' } | Export-Csv -Path D:\github\WindowsTest\$filename_AppList -NoTypeInformation
 Remove-Item "D:\github\WindowsTest\AppList.csv"
 
 
 Get-Hotfix | Export-Csv -Path D:\github\WindowsTest\UpdateList.csv -NoTypeInformation
 
-if (Test-Path D:\github\WindowsTest\D:\github\WindowsTest\$filename_UpdateList.csv) {
-  Write-Verbose -Message "Deleting the duplicate file ($filename_UpdateList.csv)" -Verbose
-  git rm D:\github\WindowsTest\D:\github\WindowsTest\$filename_UpdateList.csv
-  git commit -m "Remove the duplicate file ($filename_UpdateList.csv)"
+
+if (Test-Path D:\github\WindowsTest\$filename_UpdateList) {
+  Write-Verbose -Message "Deleting the duplicate file ($filename_UpdateList)" -Verbose
+  Set-Location D:\github\WindowsTest
+  git rm "$filename_UpdateList"
+  git commit -m "Remove the duplicate file ($filename_UpdateList)"
   git push
-  #Remove-Item D:\github\WindowsTest\D:\github\WindowsTest\$filename_UpdateList.csv
 }
 
-Import-Csv -Path "D:\github\WindowsTest\UpdateList.csv" | Where-Object { $_.PSObject.Properties.Value -ne '' } | Export-Csv -Path D:\github\WindowsTest\$filename_UpdateList.csv -NoTypeInformation
+
+Import-Csv -Path "D:\github\WindowsTest\UpdateList.csv" | Where-Object { $_.PSObject.Properties.Value -ne '' } | Export-Csv -Path D:\github\WindowsTest\$filename_UpdateList -NoTypeInformation
 Remove-Item "D:\github\WindowsTest\UpdateList.csv"
 
-
-
-git add .\$filename_AppList.csv
-git add .\$filename_UpdateList.csv
+git add $filename_AppList
+git add $filename_UpdateList
 git commit -m "Yiwei added $filename_AppList & $filename_UpdateList"
 git push
+
+
+git remote rm origin
+Set-Location D:\github
+Remove-Item D:\github\WindowsTest -Recurse -Force
+
+Set-Location D:\github\PRDCA
+git remote rm origin
+Set-Location D:\github
+Remove-Item D:\github\PRDCA -Recurse -Force
+
+
+
+
 
